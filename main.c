1bed1c4a07a6fb746f1582b78e4ed4cbf2a239f5
Daniel Iveson <c3330408@uon.edu.au>
2019-04-04 08:59:42 +0000

    First lab. Added a substitution encoder.

--- main.c
+++ main.c
@@ -1,8 +1,68 @@
 #include <stdio.h>
 
+
+
 int main()
-{
-    printf("Hello World");
+{ 
+    
+    
+    
+    //encrypting
+    while (1)
+    {
+        char str[1024];
+        char c;
+        int i = 0;
+        scanf("%c", &c);
+        
+        if (c == '\n')
+        {
+            str[i] = '\0';
+            break;
+        }
+        
+        str[i] = c;
+
+        if (c != ' ')
+        {
+            c = c - 4;
+        }
+
+            i++;
+            printf("%c", c);   
+        
+    }
+    printf("\n");
+    
+    
+    //decrypting
+    //use the spelling check to get a sentence with no errors.
+    
+    while (1)
+    {
+        char str[1024]; //won't work for later functions
+        char c;
+        int i = 0;
+        scanf("%c", &c);
+        
+        if(c == '\n')
+        {
+            str[i] = '\n';
+            break;
+        }
+        str[i] = c; // sentence is stored in a string
+        i ++;
+        
+        
+        
+    }
+    for(N = 0; N < 26; N++)
+        {
+            i = 0;
+            str[i] = str[i] + 1;
+            printf("%s\n", str);
+        }
+    
 
     return 0;
 }
